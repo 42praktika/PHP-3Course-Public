5cea35c9b3caaecfad40b48bd9aba1d33e5a9e0b
Ilya.Kosharskiy <elirector@gmail.com>
2021-10-14 15:48:35 +0300

    Generator-based coroutines

--- index.php
+++ index.php
@@ -1,53 +1,49 @@
 <?php
-
-require_once "vendor/autoload.php";
 spl_autoload_register(function ($className) {require_once './app/classes/'.$className.'.inc';});
 
-$url = "http://example.com";
-$client = new GuzzleClient($url);
-try {
-    $content = $client->get('http://gimsyaroslavl.narod.ru/Rescuer/Rescuers_Guidebook/ch143_flood.htm');
-}
-catch (UnsuccessfulRequestException $ure) {
-    throw new Exception($ure->getMessage()." Code: ".$ure->getCode()." Http message: ".$ure->getHttpReason());
-}
-catch (NotInitializedException $nie) {
-    throw new Exception("Client not initialized for some mysterious reason");
+function getTaskId(): SystemCall {
+    return new SystemCall(function (Task $task, Scheduler $scheduler) {
+       $task->setSendValue($task->getTaskId());
+       $scheduler->enqueue($task);
+
+    });
 }
 
-$parser = new UriParser($content);
-$result = $parser->getResult();
-$host = "localhost";
-$port = 5432;
-$dbname = "news";
-$user = "student";
-$password = "password";
-$uridb = new UriDB($host, $port, $dbname, $user, $password);
-$parent = $uridb->Add(new Uri($url));
-foreach ($result as $uri) {
-  $uridb->Add(new Uri($uri, null, $parent));
-}
-$dbresult = $uridb->GetAllUris();
-echo Renderer::render('uris', ['uris'=> $dbresult]);
-/*
+function addTask(Generator $coroutine) {
+    return new SystemCall(function (Task $task, Scheduler $scheduler) use ($coroutine) {
+        $task->setSendValue($scheduler->newTask($coroutine));
+        $scheduler->enqueue($task);
 
-$conn = sprintf(
-    "pgsql:host=%s;port=%s;dbname=%s;user=%s;password=%s",
-    $host,
-    $port,
-    $dbname,
-    $user,
-    $password
-);
-$pdo = new PDO($conn);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$statement = $pdo->prepare("INSERT INTO uris(uri, parent_id) VALUES(:uri, :parent_id)");
-
-foreach ( $parser->getResult() as $uri) {
-    $statement->execute(["uri" =>$uri, "parent_id" => null]);
+    });
 }
 
+function killTask(int $id) {
+    return new SystemCall(function (Task $task, Scheduler $scheduler) use ($id) {
+        $task->setSendValue($scheduler->killTask($id));
+        $scheduler->enqueue($task);
+    });
+}
 
-$query = $pdo->query("SELECT uri FROM uris");
-*/
+function childTask() {
+    $tid = (yield getTaskId());
+    while (true) {
+        echo "Child task $tid still alive!\n";
+        yield;
+    }
+}
+
+function task() {
+    $tid = (yield getTaskId());
+    $childTid = (yield addTask(childTask()));
+
+    for ($i = 1; $i <= 6; ++$i) {
+        echo "Parent task $tid iteration $i.\n";
+        yield;
+
+        if ($i == 3) yield killTask($childTid);
+    }
+}
+
+$scheduler = new Scheduler;
+$scheduler->newTask(task());
+$scheduler->run();
